b81fd186bdb1a754d7934f8e54704d1e80157f41
Centauria <centauriavc@gmail.com>
2021-05-27 07:43:32 +0800

    add xpolar

--- util.py
+++ util.py
@@ -16,3 +16,10 @@ def polar(mic_array_location, sources_location):
     inclination = np.arccos(xyz[:, 2] / r)
     azimuth = np.arccos(xyz[:, 0] / np.sqrt(xyz[:, 0] ** 2 + xyz[:, 1] ** 2))
     return np.asarray((r, inclination, azimuth)).T
+
+
+def x_linear_polar(mic_array_location, sources_location):
+    xyz = cartesian(mic_array_location, sources_location)
+    r = np.linalg.norm(xyz, axis=1)
+    alpha = np.arccos(xyz[:, 0] / r)
+    return np.asarray(alpha)
