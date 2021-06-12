ebe8661d27e68cf8e6cc0bd3bb98673b893a96a9
Yuki-jy <13865700633@163.com>
2021-06-12 15:52:09 +0800

    complete validate

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch.optim
 import torch.utils.data
 import torch.distributed as dist
 from torch.autograd import Variable
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from datetime import datetime
 
 import os
 import numpy as np
@@ -23,15 +23,65 @@ from scipy.special import perm
 
 plt.switch_backend('agg')
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 seed = np.random.randint(0, 1000)
 np.random.seed(seed)
 torch.random.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 
 
+def PIT(doa, target, loc, prob):
+    """
+    get PIT doa
+    :param doa: predict doa of speakers  [nBatch, 6]
+    :param target: predict prob of speakers  [nBatch, 6]
+    :param loc: label of speakers  [nBatch, 6]
+    :param prob: probability of speakers  [nBatch, 6]
+    :return: active doa after pit  [nBatch, 6]
+    """
+    Divide_criterion = nn.MSELoss(reduction='mean')
+    Prob_criterion = nn.BCEloss(weight=None, size_average=None, reduce=None, reduction='mean')
+
+    if doa.shape[0] == target.shape[0]:
+        nBatch = doa.shape[0]
+    active_speakers_number = (target == 1).sum(dim=1)  # active number of each batch  [nBatch]
+    count = perm(6, active_speakers_number)  # [nBatch]
+    active_doa = torch.empty((nBatch, 6))
+    for iBatch in range(nBatch):
+        cnt = int(count[iBatch])  # int(A(6, n))
+        pad_loc = torch.repeat_interleave(loc[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
+        pad_prob = torch.repeat_interleave(prob[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
+        true_active_doa = torch.empty((cnt, 6))  # [cnt, 6]
+        true_active_target = torch.repeat_interleave(target[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
+        pit_doa_loss = []
+        pit_target_loss = []
+        ip = 0
+        for p in permutations(doa[iBatch], int(
+                active_speakers_number[iBatch])):  # A(6,active_speakers_number[iBatch]){doa[iBatch]}=cnt
+            p = torch.tensor(list(p))  # p:one sequence in A(6, n), len(p)=n
+            for i, probility in enumerate(target[iBatch]):
+                index = 0
+                if probility == 1:  # fill (target != 0) with sequence in permutations
+                    true_active_doa[ip][i] = p[index]
+                else:
+                    true_active_doa[ip][i] = 0
+            pit_doa = Divide_criterion(Variable(true_active_doa[ip]).float(), Variable(pad_loc[ip]).float())
+            pit_target = Prob_criterion(Variable(true_active_target[ip]).float(), Variable(pad_prob[ip]).float())
+            pit_doa_loss.append(pit_doa)  # list
+            pit_target_loss.append(pit_target)  # len(pit_target_loss)=len(pit_doa_loss)=cnt
+            ip += 1
+        pit_loss = torch.add(torch.tensor(pit_doa_loss),
+                             torch.tensor(pit_target_loss))  # tensorlenth(pit_loss)=cnt for iBatch
+        pit_index = torch.argmin(pit_loss)  # active_doa index for  iBatch
+        active_doa[iBatch] = true_active_doa[pit_index]
+    return active_doa
+
+
 def train():
     parser = argparse.ArgumentParser(prog='train', description="""Script to train a DOA estimator""")
-    parser.add_argument("--datasets", "-d", required=True, help="Directory where datasets are", type=str)
+    parser.add_argument("--train_datasets", "-t", required=True, help="Directory where train datasets are", type=str)
+    parser.add_argument("--val_datasets", "-v", required=True, help="Directory where valid datasets are", type=str)
     parser.add_argument("--outputs", "-o", required=True, help="Directory to write results", type=str)
     parser.add_argument("--batchsize", "-b", type=int, default=256, help="Choose a batchsize")
     parser.add_argument("--epochs", "-e", type=int, default=50, help="Number of epochs")
@@ -44,6 +94,8 @@ def train():
     epochs = args.epochs
     batchsize = args.batchsize
     loss = args.loss
+    train_datasets = args.train_datatsets
+    val_datasets = args.val_datasets
     foldername = '{}_batch{}'.format(args.loss, batchsize)
     outpath = os.path.join(args.outputs, foldername)
     if not os.path.exists(outpath):
@@ -54,13 +106,17 @@ def train():
     # feature shape:(Batch, 6, 25, 513)
     # loc shape:(Batch, 6)
     # prob shape :(Batch, 6)
-    train_data = GenDOA(args.datasets, loss_type=args.loss)  # feature, loc, prob
-    train_data_entries, val_data_entries = train_test_split(train_data, test_size=0.3, random_state=11)
-    train_loader = torch.utils.data.DataLoader(train_data_entries, batch_size=batchsize, shuffle=True, drop_last=True)
-    val_loader = torch.utils.data.DataLoader(val_data_entries, batch_size=batchsize, shuffle=True, drop_last=True)
+    train_data = GenDOA(train_datasets, loss_type=args.loss)  # feature, loc, prob
+    val_data  = GenDOA(val_datasets, loss_type=args.loss)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batchsize, shuffle=True, drop_last=True)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batchsize, shuffle=True, drop_last=True)
 
     # initialize model
-    crnn = CRNN()
+    crnn = CRNN().to(device)
+
+    train_loss = []
+    valid_loss = []
+    min_valid_loss = np.inf
 
     # scheduler
     Divide_criterion = nn.MSELoss(reduction='mean')
@@ -70,6 +126,8 @@ def train():
 
     # train
     for epoch in range(epochs):
+        total_train_loss = []
+        crnn.train()
         scheduler.step()
 
         # batch data entries
@@ -81,51 +139,17 @@ def train():
             # feature: (batchsize, 6, 25, 513)
             # loc: (batchsize, 6)
             # prob: (batchsize, 6)
-            feature, loc, prob = Variable(feature), Variable(loc), Variable(prob)
 
             # forward, backward, update weights
             optimizer.zero_grad()
-            predictions = crnn(feature)
+            predictions = crnn(Variable(feature))
             doa, target = zip(*predictions)
             # doa_loss = Divide_criterion(doa, loc)
             # target_loss = Prob_criterion(target, prob)
             # compute loss
             # loss = doa_loss + target_loss
 
-            # PIT loss
-            # get PIT doa, target
-            # doa: predict doa of speakers  [nBatch, 6]
-            # target: predict prob of speakers  [nBatch, 6]
-            if doa.shape[0] == target.shape[0]:
-                nBatch = doa.shape[0]
-            active_speakers_number = (target==1).sum(dim=1)  # active number of each batch  [nBatch]
-            count = perm(6, active_speakers_number)  # [nBatch]
-            active_doa = torch.empty((nBatch, 6))
-            for iBatch in range(nBatch):
-                cnt = int(count[iBatch])  # int(A(6, n))
-                pad_loc = torch.repeat_interleave(loc[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
-                pad_prob = torch.repeat_interleave(prob[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
-                true_active_doa = torch.empty((cnt, 6))  # [cnt, 6]
-                true_active_target = torch.repeat_interleave(target[iBatch].unsqueeze(dim=0), repeats=cnt, dim=0)  # [cnt, 6]
-                pit_doa_loss = []
-                pit_target_loss = []
-                ip = 0
-                for p in permutations(doa[iBatch], int(active_speakers_number[iBatch])):  # A(6,active_speakers_number[iBatch]){doa[iBatch]}=cnt
-                    p = torch.tensor(list(p))  # p:one sequence in A(6, n), len(p)=n
-                    for i, probility in enumerate(target[iBatch]):
-                        index = 0
-                        if probility == 1:  # fill (target != 0) with sequence in permutations
-                            true_active_doa[ip][i] = p[index]
-                        else:
-                            true_active_doa[ip][i] = 0
-                    pit_doa = Divide_criterion(Variable(true_active_doa[ip]).float(), Variable(pad_loc[ip]).float())
-                    pit_target = Prob_criterion(Variable(true_active_target[ip]).float(), Variable(pad_prob[ip]).float())
-                    pit_doa_loss.append(pit_doa)  # list
-                    pit_target_loss.append(pit_target)   # len(pit_target_loss)=len(pit_doa_loss)=cnt
-                    ip += 1
-                pit_loss = torch.add(torch.tensor(pit_doa_loss), torch.tensor(pit_target_loss))   # tensorlenth(pit_loss)=cnt for iBatch
-                pit_index = torch.argmin(pit_loss)  # active_doa index for  iBatch
-                active_doa[iBatch] = true_active_doa[pit_index]
+            active_doa = PIT(doa, target, loc, prob)
 
             loss1 = Divide_criterion(Variable(active_doa).float(), Variable(loc).float())
             loss2 = Prob_criterion(Variable(target).float(), Variable(prob).float())
@@ -133,11 +157,41 @@ def train():
 
             loss.backward()
             optimizer.step()
+            total_train_loss.append(loss.item())
 
-    # print loss
+        # record train_loss
+        train_loss.append(np.mean(total_train_loss))
 
-    # valid
+        # validate
+        total_valid_loss = []
+        crnn.eval()
+        for i, val_data in enumerate(val_loader):
+            feature, loc, prob = val_data
+            with torch.no_grad():
+                prediction = crnn(Variable(feature))  # rnn output
+                doa, target = zip(*prediction)
+            active_doa = PIT(doa, target, loc, prob)
+            loss1 = Divide_criterion(Variable(active_doa).float(), Variable(loc).float())
+            loss2 = Prob_criterion(Variable(target).float(), Variable(prob).float())
+            loss = torch.add(loss1, loss2)
+            total_valid_loss.append(loss.item())
+        valid_loss.append(np.mean(total_valid_loss))
 
+        if (valid_loss[-1] < min_valid_loss):
+            torch.save({'epoch': i, 'model': crnn, 'train_loss': train_loss,
+                        'valid_loss': valid_loss}, './LSTM.model')
+            #         torch.save(optimizer, './crnn.optim')
+            min_valid_loss = valid_loss[-1]
+
+        log_string = ('iter: [{:d}/{:d}], train_loss: {:0.6f}, valid_loss: {:0.6f}, '
+                      'best_valid_loss: {:0.6f}, lr: {:0.7f}').format((epoch + 1), epochs,
+                                                                      train_loss[-1],
+                                                                      valid_loss[-1],
+                                                                      min_valid_loss,
+                                                                      optimizer.param_groups[0]['lr'])
+        scheduler.step()
+        print(str(datetime.datetime.now() + datetime.timedelta(hours=8)) + ': ')
+        print(log_string)
 
 if __name__ == "__main__":
     train()
